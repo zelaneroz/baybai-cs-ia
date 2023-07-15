f368702fe9d6736f97db63be49543660a668873e
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-07-15 22:45:39 +0800

    Finished Criteria A.

---
+++ src/baybai.py
@@ -0,0 +1,16 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+import sqlite3
+from kivymd.uix.datatables import MDDataTable
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.boxlayout import MDBoxLayout
+class LoginScreen(MDScreen):
+    dialog=None
+
+class baybai(MDApp):
+    def build(self):
+        return
+
+test = baybai()
+test.run()
