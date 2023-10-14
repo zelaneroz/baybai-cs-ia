922c51ab9b9c2646eca29fbe9ec9ade547309268
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-10-14 20:05:40 +0800

    * Registration Function Done * Log in Function Done
    !Errors!:
    From HomeScreen, errors when user clicks Learn/Test/Translate. Error message = " AttributeError: 'NoneType' object has no attribute 'current'"

--- src/baybai.py
+++ src/baybai.py
@@ -3,6 +3,7 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 import sqlite3
 from kivymd.uix.datatables import MDDataTable
+from kivy.graphics import Ellipse
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -15,9 +16,113 @@ from kivymd.uix.textfield import  MDTextField
 from syllabify import translate, syllabify
 import pyperclip
 from kivy.clock import Clock
+from encrypt import encrypt_password, check_password
+import sqlite3
+from kivy.clock import Clock
+
+currrent_user = ""
+class database_handler:
+    def __init__(self,namedb:str):
+        self.connection = sqlite3.Connection(namedb)
+        self.cursor = self.connection.cursor()
+
+    def run_query(self, query: str):
+        self.cursor.execute(query)
+        self.connection.commit()
+    def close(self):
+        self.connection.close()
+    def search(self,query:str):
+        result = self.cursor.execute(query).fetchall()
+        return [item[0] for item in result]
+        #return result
+
+class IntroScreen(MDScreen):
+    pass
+    def signup_screen(self):
+        self.parent.current = "SignUpScreen"
+
+    def login_screen(self):
+        self.parent.current = "LoginScreen"
+
+class SignUpScreen(MDScreen):
+    def login_screen(self):
+        self.parent.current = "LoginScreen"
+
+    def popup(self,out: str):
+        self.dialog = MDDialog(text=out)
+        self.dialog.open()
+
+    def validate_register(self, uname: str, pass1: str, conpass: str):
+        db = database_handler(namedb='baybai.db')
+        popup_text = ""
+
+        if uname in db.search(f"SELECT uname FROM users WHERE uname='{uname}'"):
+            popup_text += f"Username [color=0000FF]{uname}[/color] already exists."
+        elif len(pass1) <= 6:
+            popup_text += "\nPassword needs to be longer than 6 characters."
+        elif pass1 != conpass:
+            popup_text += "\nPasswords don't match."
+        else:
+            return True
+
+        popup_text += "\nPlease try again."
+        self.popup(popup_text)
+        return False
+
+    def try_register(self):
+        db = database_handler(namedb='baybai.db')
+        db.run_query("""CREATE TABLE if not exists users(
+                        id INTEGER primary key autoincrement,
+                        name TEXT not null,
+                        uname TEXT not null,
+                        password TEXT not null);""")
+        #TAKE ALL INPUTS, VALIDATE, AND APPEND TO DATABASE
+        name,uname,pass1,con_pass = self.ids.name.text,self.ids.uname.text,self.ids.password.text,self.ids.confirm_password.text
+
+        if self.validate_register(uname, pass1, con_pass):
+            db.run_query(f"INSERT INTO USERS (name,uname,password) VALUES ('{name}','{uname}','{encrypt_password(pass1)}')")
+            self.popup("[color=ACFF3C]Registration completed. Welcome![/color]")
+            db.close()
+
+            self.ids.name.text,self.ids.uname.text,self.ids.password.text,self.ids.confirm_password.text="","","",""
+            current_user = uname
+            self.parent.current = "HomeScreen"
+
+class LoginScreen(MDScreen):
+    def popup(self,out: str):
+        self.dialog = MDDialog(text=out)
+        self.dialog.open()
+    def signup_screen(self):
+        self.parent.current = "SignUpScreen"
+
+    def move_to_home_screen(self):
+        self.parent.current = "HomeScreen"
+
+    def validate_login(self,uname:str,passwd:str):
+        db = database_handler(namedb='baybai.db')
+        popup_text = ""
+        if uname == "" or passwd == "":
+            popup_text="Please enter required fields"
+        elif uname in db.search(f"SELECT uname from users where uname='{uname}'"):
+            if check_password(passwd,db.search(f"SELECT password from users where uname='{uname}'")[0]):
+                popup_text = "Successfully logged in. Welcome!"
+                return True, popup_text
+            else:
+                popup_text = "Incorrect Password. Try again."
+        else:
+            popup_text = "User does not exist."
+        return False, popup_text
+
+    def try_login(self):
+        db = database_handler(namedb='baybai.db')
+        uname,passwd = self.ids.uname.text,self.ids.password.text
+
+        if self.validate_login(uname, passwd)[0]:
+            self.popup(self.validate_login(uname, passwd)[1])
+            Clock.schedule_once(lambda dt: self.move_to_home_screen(), 2)
 
 class HomeScreen(MDScreen):
-    dialog=None
+    print("Now in HomeScreen")
 
     def home2learn(self):
         self.parent.current = "LearnScreen"
@@ -27,6 +132,10 @@ class HomeScreen(MDScreen):
         self.parent.current = "TranslateScreen"
     def home2stats(self):
         self.parent.current = "StatsScreen"
+    def home2net(self):
+        self.parent.current = "NetworkScreen"
+
+
 
 
 class LearnScreen(MDScreen):
@@ -63,6 +172,9 @@ class TranslateScreen(MDScreen):
         baybai.backtohome(self)
 
 class StatsScreen(MDScreen):
+    dialog=None
+
+class NetworkScreen(MDScreen):
     dialog=None
 
 class stats(MDScreen):
