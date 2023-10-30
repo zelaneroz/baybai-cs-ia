8955aea8643f8349ada0adcc3e348364ad3b59fe
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-10-30 19:26:45 +0900

    - experimenting how to figure out network part.
    PRIORITIES:
    - Network Application
    
    !Errors!:
    From HomeScreen, errors when user clicks Learn/Test/Translate. Error message = " AttributeError: 'NoneType' object has no attribute 'current'"

---
+++ scratch/main.py
@@ -0,0 +1,69 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.dialog import MDDialog
+from kivy.core.window import Window
+Window.size = (375,812)
+from kivy.core.text import LabelBase
+from database_handler import  database_handler
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+
+class SignUpScreen(MDScreen):
+    def login_screen(self):
+        self.parent.current = "LoginScreen"
+
+    def popup(self,out: str):
+        self.dialog = MDDialog(text=out)
+        self.dialog.open()
+
+    def validate_register(self, uname: str, pass1: str, conpass: str):
+        db = database_handler(namedb='sample2.db')
+        print('database open')
+        popup_text = ""
+
+        if uname in db.search(f"SELECT uname FROM users WHERE uname='{uname}'"):
+            popup_text += f"Username [color=0000FF]{uname}[/color] already exists."
+        elif len(pass1) <= 6:
+            popup_text += "\nPassword needs to be longer than 6 characters."
+        elif pass1 != conpass:
+            popup_text += "\nPasswords don't match."
+        else:
+            return True
+
+        popup_text += "\nPlease try again."
+        self.popup(popup_text)
+        return False
+
+    def try_register(self):
+        db = database_handler(namedb='src/baybai.db')
+        db.run_query("""CREATE TABLE if not exists users(
+                        id INTEGER primary key autoincrement,
+                        name TEXT not null,
+                        uname TEXT not null,
+                        password TEXT not null);""")
+        #TAKE ALL INPUTS, VALIDATE, AND APPEND TO DATABASE
+        name,uname,pass1,con_pass = self.ids.name.text,self.ids.uname.text,self.ids.password.text,self.ids.confirm_password.text
+
+        if self.validate_register(uname, pass1, con_pass):
+            db.run_query(f"INSERT INTO USERS (name,uname,password) VALUES ('{name}','{uname}','{encrypt_password(pass1)}')")
+            self.popup("[color=ACFF3C]Registration completed. Welcome![/color]")
+            db.close()
+
+            self.ids.name.text,self.ids.uname.text,self.ids.password.text,self.ids.confirm_password.text="","","",""
+            current_user = uname
+            print("MOVED TO HOME SCREEN")
+
+class baybai(MDApp):
+    def backtohome(self):
+        self.parent.current='HomeScreen'
+    def build(self):
+        LabelBase.register(name='Helvetica',fn_regular='fonts/HelveticaNeue-01.ttf',fn_bold='fonts/HelveticaNeue-Bold-02.ttf',fn_bolditalic='fonts/HelveticaNeue-BoldItalic-04.ttf',fn_italic='fonts/HelveticaNeue-Italic-03.ttf')
+        LabelBase.register(name='Baybayin', fn_regular='fonts/Baybayin.ttf')
+        LabelBase.register(name='Noto', fn_regular='fonts/NotoSansTagalog.ttf')
+        screen_manager = ScreenManager()
+        return Builder.load_file('main.kv')
+
+
+test = baybai()
+test.run()
